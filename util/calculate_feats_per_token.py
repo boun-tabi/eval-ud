2395d6e824351326d2cec22ff8ba874d8d1dcbca
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-29 12:04:25 +0300

    update feats per token script to print more info

--- util/calculate_feats_per_token.py
+++ util/calculate_feats_per_token.py
@@ -13,6 +13,8 @@ feats = root.find('feats').findall('feat')
 feat_count = 0
 for feat in feats:
     feat_count += int(feat.text)
+print('Non-unique feats:', feat_count)
+print('Total tokens:', total_token_count)
 print('Feats per token:', feat_count / total_token_count)
 
 # used to calculate feats per token in treebanks UD_English-ATIS (1.0251296885857883) & UD_Turkish-ATIS (2.4460817438692097)
