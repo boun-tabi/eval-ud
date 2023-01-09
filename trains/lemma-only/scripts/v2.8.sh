34206e06aa84197a01a24019e0044430f8405fef
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-01-09 12:15:08 +0000

    add train configs for smalls

--- trains/lemma-only/scripts/v2.8.sh
+++ trains/lemma-only/scripts/v2.8.sh
@@ -2,14 +2,14 @@
 #SBATCH --ntasks=2
 #SBATCH --gpu-bind=closest
 #SBATCH --gpus=1
-#SBATCH --output=/clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/slurm/outputs/%j-steps-parser_lemma_only_baseline_v2.8.out
+#SBATCH --output=/clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/gitlab-repo/trains/slurm/outputs/%j-steps-parser_lemma_only_baseline_v2.8.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=furkanakkurt9285@gmail.com
 #SBATCH --mem=20GB
 #SBATCH -t 0-5:00
 
 env
-python3 /clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/steps-parser/src/train.py /clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/gitlab-repo/trains/lemma-only/configs/lemma_only_baseline_v2.8.json
+python3 /clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/steps-parser/src/train.py /clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/gitlab-repo/trains/lemma-only/configs/v2.8.json
 
 RET=$?
 
---
+++ trains/slurm/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --ntasks=2
+#SBATCH --gpu-bind=closest
+#SBATCH --gpus=1
+#SBATCH --output=/clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/gitlab-repo/trains/slurm/outputs/%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=furkanakkurt9285@gmail.com
+#SBATCH --mem=40GB
+#SBATCH -t 0-5:00
+
+log_path=/clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/gitlab-repo/trains/slurm/Logs.txt
+date >> $log_path
+echo $1 >> $log_path
+echo >> $log_path
+
+train_config_path=$(readlink -f $1)
+
+env
+python3 /clusterusers/furkan.akkurt@boun.edu.tr/eval-ud/steps-parser/src/train.py $train_config_path
+
+RET=$?
+
+exit $RET
