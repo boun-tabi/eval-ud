3e11f77a4cea6c6b4eec6ba78b284b8761828b0a
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-08-08 22:43:34 +0300

    correct convert to json script

--- util/convert_tb_json.py
+++ util/convert_tb_json.py
@@ -11,8 +11,8 @@ data_files = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.end
 data_files = sorted(data_files)
 
 data_l = []
-md_pattern = '#(.+)=(.+)'
-annotation_pattern = '(.+\t){9}.+'
+md_pattern = re.compile('^# (.+?) = (.+?)$')
+annotation_pattern = re.compile('(.+\t){9}.+')
 for f in data_files:
     with open(f, 'r') as f:
         content = f.read()
@@ -21,12 +21,12 @@ for f in data_files:
         lines = sent.split('\n')
         d_t = {}
         for i, line in enumerate(lines):
-            md_match = re.match(md_pattern, line)
+            md_match = md_pattern.match(line)
             if md_match:
                 field = md_match.group(1).strip()
                 value = md_match.group(2).strip()
                 d_t[field] = value
-            annotation_match = re.match(annotation_pattern, line)
+            annotation_match = annotation_pattern.match(line)
             if annotation_match:
                 annotation = '\n'.join(lines[i:])
                 d_t['table'] = annotation
