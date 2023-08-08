11c64572dc287fcdd598ea964419886c0dc6e700
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-08-08 12:57:37 +0300

    update doc script for openai and add document outputs

--- llm-approach/document.py
+++ llm-approach/document.py
@@ -2,14 +2,14 @@ import os, json
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(THIS_DIR, 'experiment_outputs')
-with open(os.path.join(output_dir, 'experiment06_output-tr_boun_v2.11-langchain.json'), 'r', encoding='utf-8') as f:
+with open(os.path.join(output_dir, 'experiment06_output-tr_boun_v2.11-openai.json'), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 doc_l = []
 for el in data:
     sent_id = el['sent_id']
     in_text = el['text']
-    out_text = el['output']['text']
+    out_text = el['output']['content']
     doc_l.append((sent_id, in_text, out_text))
 
 out_str = ''
@@ -19,5 +19,5 @@ for sent_id, in_text, out_text in doc_l:
     out_str += '- LLM Output: {}\n'.format(out_text)
     out_str += '\n---\n\n'
 
-with open(os.path.join(output_dir, 'experiment06_output-tr_boun_v2.11-langchain.txt'), 'w', encoding='utf-8') as f:
+with open(os.path.join(output_dir, 'experiment06_output-tr_boun_v2.11-openai.txt'), 'w', encoding='utf-8') as f:
     f.write(out_str)
