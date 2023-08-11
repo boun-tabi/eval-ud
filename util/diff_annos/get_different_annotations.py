1d0a238e3ba90ce0ea00637a4c5c8074596f74fc
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-08-11 12:28:34 +0300

    move diff annos

--- util/diff_annos/get_different_annotations.py
+++ util/diff_annos/get_different_annotations.py
@@ -22,6 +22,9 @@ for el in second_data:
 sent_ids = list(set(f_d.keys()).union(set(s_d.keys())))
 len_sent_ids = len(sent_ids)
 change_d = {}
+function_type = 'SequenceMatcher.ratio'
+string_type = 'entire table'
+similarity_type = '{} between entire table strings ; 1.0 means identical, 0.0 means completely different'.format(function_type)
 for i, sent_id in enumerate(sent_ids):
     table1, table2 = f_d[sent_id], s_d[sent_id]
     ratio = SequenceMatcher(None, table1, table2).ratio()
@@ -29,6 +32,6 @@ for i, sent_id in enumerate(sent_ids):
     if i % 1000 == 0:
         print('Remaining: {} / {}'.format(len_sent_ids - i, len_sent_ids))
 # sort dict
-change_d = {k: v for k, v in sorted(change_d.items(), key=lambda item: item[1], reverse=True)}
-with open(os.path.join(THIS_DIR, 'different_annotations.json'), 'w', encoding='utf-8') as f:
+change_d = {'similarity_type': similarity_type, 'ratios': {k: v for k, v in sorted(change_d.items(), key=lambda item: item[1], reverse=True)}}
+with open(os.path.join(THIS_DIR, 'different_annotations-{}-{}.json'.format(function_type, string_type)), 'w', encoding='utf-8') as f:
     json.dump(change_d, f, ensure_ascii=False, indent=4)
