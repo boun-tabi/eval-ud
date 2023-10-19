b77850b65ccacdbff35d440c7a49ace8b7605f3b
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-10-19 04:11:14 +0300

    continue implementation of error analysis based on annotations

--- llm-approach/do_error_analysis-annotation.py
+++ llm-approach/do_error_analysis-annotation.py
@@ -12,7 +12,7 @@ def main():
     summary_path = os.path.join(dir, 'summary.json')
     with open(summary_path, 'r', encoding='utf-8') as f:
         summary = json.load(f)
-    
+
     md_path = os.path.join(dir, 'md.json')
     with open(md_path, 'r', encoding='utf-8') as f:
         md = json.load(f)
@@ -27,13 +27,13 @@ def main():
         del el['sent_id']
         tb8[sent_id] = el
     with open(tb11_path, 'r', encoding='utf-8') as f:
-        tb11 = json.load(f)
+        tb11_temp = json.load(f)
     tb11 = {}
-    for el in tb11:
+    for el in tb11_temp:
         sent_id = el['sent_id']
         del el['sent_id']
-        tb11[sent_id].update(el)
-    
+        tb11[sent_id] = el
+
     if 'results' not in summary:
         print('No results found in file')
         return
@@ -41,8 +41,52 @@ def main():
 
     analysis_d = {'sentences': {}}
     for sent_id, value in results.items():
+        v2_8_table, v2_11_table = tb8[sent_id]['table'], tb11[sent_id]['table']
+        v2_8_annotation = {}
+        for line in v2_8_table.split('\n'):
+            fields = line.split('\t')
+            id_t, form_t, feats_t, misc_t = fields[0], fields[1], fields[5], fields[9]
+            v2_8_annotation[id_t] = {'form': form_t, 'space_after': True, 'feats': feats_t}
+            if 'SpaceAfter=No' in misc_t:
+                v2_8_annotation[id_t]['space_after'] = False
+
+        v2_11_annotation = {}
+        for line in v2_11_table.split('\n'):
+            fields = line.split('\t')
+            id_t, form_t, feats_t, misc_t = fields[0], fields[1], fields[5], fields[9]
+            v2_11_annotation[id_t] = {'form': form_t, 'space_after': True, 'feats': feats_t}
+            if 'SpaceAfter=No' in misc_t:
+                v2_11_annotation[id_t]['space_after'] = False
+
         ratio_v2_8, ratio_v2_11 = value['v2.8 ratio'], value['v2.11 ratio']
         original_text, text_v2_8, text_v2_11 = value['original text'], value['v2.8 text'], value['v2.11 text']
+
+        id_cursor8, id_cursor11 = 1, 1
+        for token in original_text.split(' '):
+            if '{}-{}'.format(id_cursor8, id_cursor8 + 1) in v2_8_annotation.keys():
+                feats1_str = v2_8_annotation[id_cursor8]['feats']
+                feats1_d = {}
+                for feat in feats1_str.split('|'):
+                    feat_k, feat_v = feat.split('=')
+                    feats1_d[feat_k] = feat_v
+                feats2_str = v2_8_annotation[id_cursor8 + 1]['feats']
+                feats2_d = {}
+                for feat in feats2_str.split('|'):
+                    feat_k, feat_v = feat.split('=')
+                    feats2_d[feat_k] = feat_v
+                feats1_set = set(feats1_d.keys())
+                key_l = feats1_set.union(set(feats2_d.keys()))
+                if len(key_l) != len(feats1_set) + len(feats2_d.keys()):
+                    print('Error in annotation')
+                    input()
+                id_cursor8 += 1
+            if '{}-{}'.format(id_cursor11, id_cursor11 + 1) in v2_11_annotation.keys():
+                feats1 = v2_11_annotation[id_cursor11]['feats']
+                feats2 = v2_11_annotation[id_cursor11 + 1]['feats']
+                id_cursor11 += 1
+            id_cursor8 += 1
+            id_cursor11 += 1
+
         diff = ratio_v2_11 - ratio_v2_8
         d = {'diff': diff, 'original_text': original_text, 'text_v2_8': text_v2_8, 'text_v2_11': text_v2_11}
         analysis_d['sentences'][sent_id] = d
