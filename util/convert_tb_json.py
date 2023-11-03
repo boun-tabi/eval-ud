7b94567f24af7d96d6aa774a7057f95dcf26d2e7
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-11-03 09:49:09 +0300

    update tb jsons

--- util/convert_tb_json.py
+++ util/convert_tb_json.py
@@ -10,7 +10,7 @@ data_path = args.treebank
 data_files = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.conllu')]
 data_files = sorted(data_files)
 
-data_l = []
+data_d = {}
 md_pattern = re.compile('^# (.+?) = (.+?)$')
 annotation_pattern = re.compile('(.+\t){9}.+')
 for f in data_files:
@@ -19,20 +19,24 @@ for f in data_files:
     sents = content.split('\n\n')
     for sent in sents:
         lines = sent.split('\n')
+        sent_id = ''
         d_t = {}
         for i, line in enumerate(lines):
             md_match = md_pattern.match(line)
             if md_match:
                 field = md_match.group(1).strip()
                 value = md_match.group(2).strip()
-                d_t[field] = value
+                if field == 'sent_id':
+                    sent_id = value
+                else:
+                    d_t[field] = value
             annotation_match = annotation_pattern.match(line)
             if annotation_match:
                 annotation = '\n'.join(lines[i:])
                 d_t['table'] = annotation
                 break
         if d_t:
-            data_l.append(d_t)
+            data_d[sent_id] = d_t
 
 with open(os.path.join(data_path, 'treebank.json'), 'w', encoding='utf-8') as f:
-    json.dump(data_l, f, ensure_ascii=False, indent=4)
+    json.dump(data_d, f, ensure_ascii=False, indent=4)
