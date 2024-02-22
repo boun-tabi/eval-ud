3cecec2787c0a94835b23ed0078c084da4e18939
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-02-23 00:02:36 +0300

    Add order field to meta.json and gather sentence reconstructions

--- llm-approach/manual-reconstruction/sentence-based-task/scripts/gather-constructions.py
+++ llm-approach/manual-reconstruction/sentence-based-task/scripts/gather-constructions.py
@@ -1,14 +1,16 @@
-import argparse, csv, json
+import argparse, csv, json, re
 from pathlib import Path
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sheet-dir', type=str, required=True)
     parser.add_argument('-t', '--type', type=str, required=True)
+    parser.add_argument('-o', '--order', type=str)
     return parser.parse_args()
 
 def main():
     args = get_args()
+    order = args.order
     sheet_dir = Path(args.sheet_dir)
     task_type = args.type
     md_path = sheet_dir / 'meta.json'
@@ -16,18 +18,36 @@ def main():
         md = json.load(f)
     relevant_keys = [k for k, v in md.items() if v['type'] == task_type]
     people = {v['person'] for k, v in md.items() if k in relevant_keys}
-    versions = {v['version'] for k, v in md.items() if k in relevant_keys}
+    versions = {v['version'] for k, v in md.items() if k in relevant_keys if order is None or v['order'] == int(order)}
     task_names = [v['task_name'] for k, v in md.items() if k in relevant_keys]
 
-    constructions = {person: {version: [] for version in versions} for person in people}
+    task_id_pattern = re.compile('^(\d+) - (.*) - .*$')
+
+    constructions = {person: {version: {} for version in versions} for person in people}
     for sheet in sheet_dir.iterdir():
-        if sheet.suffix == '.csv' and sheet.stem in task_names:
+        if sheet.suffix != '.csv':
+            continue
+        sheet_name = sheet.stem
+        task_id_match = task_id_pattern.match(sheet_name)
+        if not task_id_match:
+            continue
+        task_number = task_id_match.group(1)
+        if order:
+            if not 'order' in md[task_number]:
+                continue
+            order_t = md[task_number]['order']
+            if order_t != int(order):
+                continue
+        person, version = md[task_number]['person'], md[task_number]['version']
+        sheet_name = task_id_match.group(2)
+        if sheet_name in task_names:
             with open(sheet, 'r', encoding='utf-8') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    print(row)
-                    input()
-
+                    sent_id, text = row['Sentence ID'], row['Text']
+                    constructions[person][version][sent_id] = text
+    with open(sheet_dir / f'{args.type}-{args.order}.json', 'w', encoding='utf-8') as f:
+        json.dump(constructions, f, ensure_ascii=False, indent=2)
 
 if __name__ == '__main__':
     main()
