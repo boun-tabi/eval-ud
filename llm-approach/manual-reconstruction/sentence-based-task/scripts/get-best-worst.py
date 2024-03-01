bb1afbff9f2a85f4e435bcb12e16b3f6b57ceb00
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-03-01 14:33:42 +0300

    fix get-best-worst script and outputs

--- llm-approach/manual-reconstruction/sentence-based-task/scripts/get-best-worst.py
+++ llm-approach/manual-reconstruction/sentence-based-task/scripts/get-best-worst.py
@@ -53,12 +53,12 @@ def get_accuracy(original_text, output_text, tokenizer):
             correct += 1
     return correct, len(original_tokens)
 
-def get_best_worst(accuracies, output_d):
+def get_best_worst(accuracies, output_d, type_t):
     accuracies.sort(reverse=True)
     best_l, worst_l = [], []
     for i, (accuracy, sent_id, version) in enumerate(accuracies):
         if i < 10 or i >= len(accuracies) - 10:
-            d = {'sent_id': sent_id, 'version': version, 'accuracy': accuracy, 'original_text': output_d[sent_id]['original_text'], version: output_d[sent_id][version]}
+            d = {'sent_id': sent_id, 'version': version, 'accuracy': accuracy, 'original_text': output_d[sent_id]['original_text'], version: output_d[sent_id][type_t][version]}
             if i < 10:
                 best_l.append(d)
             else:
@@ -96,14 +96,14 @@ def main():
         v2_p1 = constructions_d[person1][version2][sent_id]
         v1_p2 = constructions_d[person2][version1][sent_id]
         v2_p2 = constructions_d[person2][version2][sent_id]
-        output_d[sent_id] = {'original_text': original_text, version1: v1_llm, version2: v2_llm, person1: {version1: v1_p1, version2: v1_p2}, person2: {version1: v2_p1, version2: v2_p2}}
+        output_d[sent_id] = {'original_text': original_text, 'llm': {version1: v1_llm, version2: v2_llm}, person1: {version1: v1_p1, version2: v1_p2}, person2: {version1: v2_p1, version2: v2_p2}}
 
     nlp = Turkish()
     tokenizer = nlp.tokenizer
 
     comparison_d = {sent_id: {'llm': {version1: {'correct': 0, 'all': 0}, version2: {'correct': 0, 'all': 0}}, person1: {version1: {'correct': 0, 'all': 0}, version2: {'correct': 0, 'all': 0}}, person2: {version1: {'correct': 0, 'all': 0}, version2: {'correct': 0, 'all': 0}}} for sent_id in sent_ids}
     for sent_id in sent_ids:
-        original_text, text_v1_llm, text_v2_llm = output_d[sent_id]['original_text'], output_d[sent_id][version1], output_d[sent_id][version2]
+        original_text, text_v1_llm, text_v2_llm = output_d[sent_id]['original_text'], output_d[sent_id]['llm'][version1], output_d[sent_id]['llm'][version2]
         if text_v1_llm[0] == '"' and text_v1_llm[-1] == '"' and original_text[0] != '"' and original_text[-1] != '"':
             text_v1_llm = text_v1_llm[1:-1]
         if text_v2_llm[0] == '"' and text_v2_llm[-1] == '"' and original_text[0] != '"' and original_text[-1] != '"':
@@ -131,9 +131,9 @@ def main():
                     p2_accuracies.append((accuracy, sent_id, version))
 
     best_worst_d = {'llm': {'best': [], 'worst': []}, person1: {'best': [], 'worst': []}, person2: {'best': [], 'worst': []}}
-    best_worst_d['llm']['best'], best_worst_d['llm']['worst'] = get_best_worst(llm_accuracies, output_d)
-    best_worst_d[person1]['best'], best_worst_d[person1]['worst'] = get_best_worst(p1_accuracies, output_d)
-    best_worst_d[person2]['best'], best_worst_d[person2]['worst'] = get_best_worst(p2_accuracies, output_d)
+    best_worst_d['llm']['best'], best_worst_d['llm']['worst'] = get_best_worst(llm_accuracies, output_d, 'llm')
+    best_worst_d[person1]['best'], best_worst_d[person1]['worst'] = get_best_worst(p1_accuracies, output_d, person1)
+    best_worst_d[person2]['best'], best_worst_d[person2]['worst'] = get_best_worst(p2_accuracies, output_d, person2)
 
     with open(llm_dir / 'best-worst.json', 'w', encoding='utf-8') as f:
         json.dump(best_worst_d, f, ensure_ascii=False, indent=4)
