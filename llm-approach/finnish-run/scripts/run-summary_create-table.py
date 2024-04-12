a0586c2c93419e963d8edebc900b3eeee12c4cca
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-04-12 17:16:29 +0300

    delete prev runs

--- llm-approach/finnish-run/scripts/run-summary_create-table.py
+++ llm-approach/finnish-run/scripts/run-summary_create-table.py
@@ -6,8 +6,8 @@ run_dirs = [
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Basque-BDT-2.11-2024-04-10_17-48-28',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_English-EWT-2.12-2024-04-10_18-10-28',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_English-EWT-2.13-2024-04-10_18-11-38',
-    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.8-2024-04-10_18-44-56',
-    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.11-2024-04-10_18-47-10',
+    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.8-2024-04-11_14-00-08',
+    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.11-2024-04-11_14-03-02',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Irish-IDT-2.7-2024-04-10_19-04-12',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Irish-IDT-2.8-2024-04-10_19-05-36',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Highland_Puebla_Nahuatl-ITML-2.13-2024-04-10_21-02-09',
@@ -20,11 +20,11 @@ pass_l = [
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Basque-BDT-2.11-2024-04-10_17-48-28',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_English-EWT-2.12-2024-04-10_18-10-28',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_English-EWT-2.13-2024-04-10_18-11-38',
-    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.8-2024-04-10_18-44-56',
-    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Turkish-BOUN-2.11-2024-04-10_18-47-10',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Irish-IDT-2.7-2024-04-10_19-04-12',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Irish-IDT-2.8-2024-04-10_19-05-36',
     'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Highland_Puebla_Nahuatl-ITML-2.13-2024-04-10_21-02-09',
+    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Hindi-HDTB-2.9-2024-04-10_21-06-07',
+    'llm-approach/finnish-run/scripts/outputs/poe_GPT-4-UD_Hindi-HDTB-2.10-2024-04-10_21-14-11'
 ]
 
 def main():
--- llm-approach/finnish-run/scripts/summary-table.md
+++ llm-approach/finnish-run/scripts/summary-table.md
@@ -9,5 +9,5 @@
 | UD_Hindi-HDTB | 2.10 | GPT-4 | 94.2% | 72.6% | Yes | 500 |
 | UD_Irish-IDT | 2.7 | GPT-4 | 93.7% | 74.0% | Yes | 500 |
 | UD_Irish-IDT | 2.8 | GPT-4 | 93.8% | 73.1% | Yes | 500 |
-| UD_Turkish-BOUN | 2.8 | GPT-4 | 88.8% | 62.6% | Yes | 500 |
-| UD_Turkish-BOUN | 2.11 | GPT-4 | 90.1% | 66.2% | Yes | 500 |
+| UD_Turkish-BOUN | 2.8 | GPT-4 | 88.3% | 60.3% | Yes | 500 |
+| UD_Turkish-BOUN | 2.11 | GPT-4 | 88.7% | 63.2% | Yes | 500 |
