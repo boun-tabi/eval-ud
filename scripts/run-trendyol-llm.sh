8c44121ead9a382b16b1785a209a77cb712ec271
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-04-28 09:09:55 +0300

    add 2.8 run

--- scripts/run-trendyol-llm.sh
+++ scripts/run-trendyol-llm.sh
@@ -4,14 +4,13 @@
 #SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
 #SBATCH --time=7-00:00:00
 #SBATCH --gpus=1
-#SBATCH --cpus-per-gpu=8
-#SBATCH --mem-per-gpu=80G
+#SBATCH --cpus-per-gpu=12
+#SBATCH --mem-per-gpu=60G
 
 source /opt/python3/venv/base/bin/activate
 
 cd ~/eval-ud
 pip install -r llm-approach/requirements.txt
-pip install sentencepiece protobuf==3.20.0
 
-python3 llm-approach/finnish-run/scripts/run-llm-experiment.py -r llm-approach/finnish-run/scripts/outputs/trendyol_Trendyol-LLM-7b-chat-v1.0-UD_Turkish-BOUN-2.8-2024-04-27_16-49-11
+python3 llm-approach/finnish-run/scripts/run-llm-experiment.py --sent-count 500 -d util/ud-docs -lp llm-approach/data/langs.json -l tr -v 2.11 --data-dir llm-approach/finnish-run/data -tb UD_Turkish-BOUN -m trendyol_Trendyol-LLM-7b-chat-v1.0
 
