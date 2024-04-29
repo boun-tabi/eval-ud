246980e2d05474b1698ef72f79ecae4e634d0fbb
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-04-29 10:18:00 +0300

    add 2.11 trendyol run with dependencies

--- scripts/run-trendyol-llm.sh
+++ scripts/run-trendyol-llm.sh
@@ -12,5 +12,5 @@ source /opt/python3/venv/base/bin/activate
 cd ~/eval-ud
 pip install -r llm-approach/requirements.txt
 
-python3 llm-approach/finnish-run/scripts/run-llm-experiment.py --sent-count 500 -d util/ud-docs -lp llm-approach/data/langs.json -l tr -v 2.11 --data-dir llm-approach/finnish-run/data -tb UD_Turkish-BOUN -m trendyol_Trendyol-LLM-7b-chat-v1.0
+python3 llm-approach/finnish-run/scripts/run-llm-experiment.py --sent-count 500 -d util/ud-docs -lp llm-approach/data/langs.json -l tr -v 2.11 --data-dir llm-approach/finnish-run/data -tb UD_Turkish-BOUN -m trendyol_Trendyol-LLM-7b-chat-v1.0 --has-dependency
 
