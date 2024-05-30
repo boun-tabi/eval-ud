838cc21a71f6518b63d1ef5afc22676663cc10aa
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-05-30 22:16:50 +0300

    add debugging to script

--- scripts/run-rem-experiments.sh
+++ scripts/run-rem-experiments.sh
@@ -9,6 +9,7 @@ source /opt/python3/venv/base/bin/activate
 cd ~/eval-ud
 pip install poetry
 poetry install
+echo "Dependencies installed"
 
 main_dir="llm-approach/finnish-run/scripts/outputs"
 
@@ -22,12 +23,16 @@ do
     # if file does not exist
     if [ ! -f $tb_output_file ]
     then
+        echo 'File does not exist for' $dir
+        echo Running $dir
         python3 llm-approach/finnish-run/scripts/run-llm-experiment.py -r $dir
     fi
     sent_id_count=$(cat $tb_output_file | grep sent_id | wc -l)
     # if not 500
     if [ $sent_id_count -ne 500 ]
     then
+        echo 'Sent ID count is not 500 for' $dir
+        echo Running $dir
         python3 llm-approach/finnish-run/scripts/run-llm-experiment.py -r $dir
     fi
 done
