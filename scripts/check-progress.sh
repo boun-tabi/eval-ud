118a31bc074fcfaa3eb57068212998390fa9316b
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-05-30 22:10:13 +0300

    convert requirements to poetry toml file ; update check progress script to check all output dirs

--- scripts/check-progress.sh
+++ scripts/check-progress.sh
@@ -1,18 +1,26 @@
 main_dir="llm-approach/finnish-run/scripts/outputs"
-dirs=(
-    "poe_Claude-2-100k-UD_Turkish-BOUN-2.11-2024-05-28_14-07-41"
-    "poe_Claude-2-100k-UD_Turkish-BOUN-2.8-2024-05-28_13-15-11"
-    "poe_Claude-3-Opus-200k-UD_Turkish-BOUN-2.11-2024-05-28_17-04-17"
-    "poe_Claude-3-Opus-200k-UD_Turkish-BOUN-2.8-2024-05-28_15-59-41"
-    "poe_Claude-instant-100k-UD_Turkish-BOUN-2.11-2024-05-28_11-38-41"
-    "poe_Claude-instant-100k-UD_Turkish-BOUN-2.8-2024-05-28_00-37-10"
-    "poe_Llama-2-70b-UD_Turkish-BOUN-2.11-2024-05-28_18-49-41"
-    "poe_Llama-2-70b-UD_Turkish-BOUN-2.8-2024-05-28_18-07-41"
-    "poe_GPT-3.5-Turbo-UD_Turkish-BOUN-2.8-2024-05-28_22-58-45"
-)
 
+# get only directories
+dirs=($(ls -d $main_dir/*/))
+
+# iterate over all directories
 for dir in "${dirs[@]}"
 do
-    echo $dir
-    cat $main_dir/$dir/tb_output.json | grep sent_id | wc -l
+    tb_output_file="$dir/tb_output.json"
+    # if file does not exist
+    if [ ! -f $tb_output_file ]
+    then
+        echo $dir
+        echo "File does not exist"
+        echo "----------------"
+        continue
+    fi
+    sent_id_count=$(cat $tb_output_file | grep sent_id | wc -l)
+    # if not 500
+    if [ $sent_id_count -ne 500 ]
+    then
+        echo $dir
+        echo $sent_id_count
+        echo "----------------"
+    fi
 done
